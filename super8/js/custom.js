b3dd11ae2c870fd933dadb630533262a3010c3ab
Unknown <emma@codegod.com.tw>
2019-07-03 14:28:47 +0800

    [add]新增簽到功能

--- super8/js/custom.js
+++ super8/js/custom.js
@@ -1,9 +1,11 @@
-/* Theme Name:iDea - Clean & Powerful Bootstrap Theme
- * Author:HtmlCoder
- * Author URI:http://www.htmlcoder.me
- * Author e-mail:htmlcoder.me@gmail.com
- * Version:1.0.0
- * Created:October 2014
- * License URI:http://wrapbootstrap.com
- * File Description: Place here your custom scripts
- */
+//check in
+// if (document.querySelector(".modal-content_course")) {
+//     document.querySelector(".modal-content_course").onclick = function () { 
+//         toggleCourse();
+//     };
+
+//     function toggleCourse() {
+//         //開啟 course_select
+//         document.querySelector(".course").classList.toggle("course_show");
+//     }
+// }
